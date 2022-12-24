8ee40b0d0a95727057d8f1b77eb5ea4762c78490
bartdurak <projektator@gmail.com>
2022-12-24 02:48:01 +0100

    losowanie odgadywanie

--- main.cpp
+++ main.cpp
@@ -4,12 +4,24 @@
 
 using namespace std;
 
-int liczba ;
+int liczba , strzal;
 int main() {
 cout << "Witajcie ! losowanie od 1 do 100"<< endl;
 srand(time(NULL));
-liczba = rand() % 100+1;
-cout << liczba <<endl;
+    liczba = rand() % 100+1;
+    cout << liczba <<endl;
+
+    while (strzal!=liczba)
+    {
+2        cout << "Zgadnij jaka to liczba: ";
+        cin >> strzal;
+
+        if (strzal==liczba)
+            cout << "Udało sie"<< endl;
+        else if (strzal<liczba)
+            cout<< "to za mało "<<endl;
+
+    }
 
     return 0;
 }
